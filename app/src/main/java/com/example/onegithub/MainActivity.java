c48c390ec022622474b816139743fe0abec79424
zyd <zzyydd@qq.cn>
2017-11-06 17:09:08 +0800

    github的第一次提交

---
+++ app/src/main/java/com/example/onegithub/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.onegithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("我的github第一个项目  很开心啊");
+        System.out.println("哈哈");
+    }
+}
